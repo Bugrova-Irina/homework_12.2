b127b018d8a4e96df66f0791cbc048c1f66e2fb8
Bugrova Irina <bugrova147@yandex.ru>
2024-12-15 22:38:26 +0300

    add looging into utils.py, fixed masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('..//logs/masks.log', 'w', encoding='utf-8')
+file_handler = logging.FileHandler('..//homework_12.2/logs/masks.log', 'w', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
